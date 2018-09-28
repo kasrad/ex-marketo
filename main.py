443be71d40323a75c67048983fa3c53eb9f620b4
Radim Kašpárek <radim248@gmail.com>
2018-09-28 14:07:10 +0100

    functionality broadened

--- main.py
+++ main.py
@@ -178,6 +178,167 @@ def extract_leads_by_filter(output_file,
         dict_writer.writerows(leads)
         
     
+def get_companies(output_file,
+                  source_file,
+                  filter_on,
+                  filter_values_column,
+                  fields = ['id', 'firstName', 'lastName', 'email', 'updatedAt',
+                            'createdAt', 'Do Not Call Reason']):
+    """
+    filterType can be: externalCompanyId, id, externalSalesPersonId, company 
+    
+    Extracts companies based on filters 
+    source_file -  needs to contain a column with the values to input
+    to the filter
+    filter_values_column - the column in the source file that contains
+    the values to input to the filter
+    filter_on- specifies the field in API (e.g. "company")
+    fields - the fields in the API that will be returned
+    """
+        
+    with open(source_file, mode='rt', encoding='utf-8') as in_file,\
+         open(output_file, mode='w', encoding='utf-8') as out_file:
+            
+
+        companies = []
+        lazy_lines = (line for line in in_file)
+        reader = csv.DictReader(lazy_lines, lineterminator = '\n')
+        
+        filter_values_list = []
+        for company_record in reader:
+            filter_values_list.append(company_record[filter_values_column])
+               
+        companies = mc.execute(method = 'get_companies',
+                          filterType = filter_on,
+                          filterValues = filter_values_list,
+                          fields = fields,
+                          batchSize=None)
+        
+        if len(companies) > 0:
+            print('%i companies extracted', len(companies))
+        else:
+            print('No companies match the criteria!')
+        
+        keys = (fields)
+        dict_writer = csv.DictWriter(out_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(companies)
+        
+        
+        
+        
+def get_lead_activities(output_file,
+                        source_file,
+                        since_date,
+                        until_date):
+    
+    """
+    source file: has to contain columns 'activity_type_ids' and 'lead_ids'. These
+    must contain the values corresponding for the query.
+    output file: will contain columns based on the fields in extracted responses - it can definitely happen that different runs
+                    will produce different number of columns!!    
+    since_date
+    until_date
+    """
+        
+    activity_type_ids = list(pd.read_csv(source_file,
+                                         skipinitialspace=True,
+                                         usecols=['activity_type_ids']).iloc[:,0])
+    lead_ids = list(pd.read_csv(source_file,
+                                 skipinitialspace=True,
+                                 usecols=['lead_ids']).iloc[:,0])
+    
+    lead_ids = [str(int(i)) for i in lead_ids if str(i) != 'nan']
+    activity_type_ids = [str(int(i)) for i in activity_type_ids if str(i) != 'nan']
+    
+    results = mc.execute(method='get_lead_activities',
+                         activityTypeIds = activity_type_ids,
+                         nextPageToken = None,
+                         sinceDatetime = since_date,
+                         untilDatetime = until_date,
+                         batchSize = None,
+                         listId = None,
+                         leadIds = lead_ids)
+    
+    if len(results) == 0:
+        print('No results!')
+        return
+    
+    unique_keys = []
+    for i in results:
+        try:
+            for j in i['attributes']:
+                i[j['name']] = j['value']
+
+            i.pop('attributes', None)
+
+            unique_keys.extend(list(i.keys()))
+        except KeyError:
+            pass
+            
+
+    unique_keys = list(set(unique_keys))
+
+    with open(output_file, mode='w', encoding='utf-8') as out_file:
+
+            keys = (unique_keys)
+            dict_writer = csv.DictWriter(out_file, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(results)
+            
+def get_lead_changes(output_file,
+                     fields,
+                     since_date,
+                     until_date):
+    
+    """
+    this function take very long to run
+    output file: will contain columns based on the fields in extracted responses - it can definitely happen that different runs
+                    will produce different number of columns!!    
+    since_date
+    until_date
+    fields: list of field names to return changes for, field names can be retrieved with the Describe Lead API
+    """
+    
+      
+    results = mc.execute(method = 'get_lead_changes',
+                         fields = fields,
+                         nextPageToken = None, 
+                         sinceDatetime = since_date,
+                         untilDatetime = until_date,
+                         batchSize = None,
+                         listId = None)
+   
+    if len(results) == 0:
+        print('No results!')
+        return
+    
+    unique_keys = []
+    for i in results:
+        try:
+            for j in i['attributes']:
+                i[j['name']] = j['value']
+
+            i.pop('attributes', None)
+            
+            for k in i['fields']:
+                i[k['name']] = k['value']
+                
+            i.pop('fields', None)
+
+            unique_keys.extend(list(i.keys()))
+        except KeyError:
+            pass
+            
+
+    unique_keys = list(set(unique_keys))
+
+    with open(output_file, mode='w', encoding='utf-8') as out_file:
+
+            keys = (unique_keys)
+            dict_writer = csv.DictWriter(out_file, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(results)
         
         
     
